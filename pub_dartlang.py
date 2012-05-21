ef7e2d81324284390390303802c15f775bd2b7e4
Nathan Weizenbaum <nweiz@google.com>
2012-05-21 13:48:26 -0700

    Add a view.

--- pub_dartlang.py
+++ pub_dartlang.py
@@ -1,10 +1,15 @@
+import os
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
+        self.response.headers['Content-Type'] = 'text/html'
+        path = os.path.join(os.path.dirname(__file__), 'views/index.html')
+        self.response.out.write(template.render(path, {}))
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage)],
