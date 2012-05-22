1cd1847b6cc35157026ec36009a0eaa3e3c4f95d
Nathan Weizenbaum <nweiz@google.com>
2012-05-21 17:43:32 -0700

    Add flash infrastructure.

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -13,9 +13,21 @@ class Base:
         content = renderer.render(
             renderer.load_template(name), *context, **kwargs)
 
+        # We're about to display the flash message, so we should get rid of the
+        # cookie containing it.
+        cherrypy.response.cookie['flash'] = ''
+        cherrypy.response.cookie['flash']['expires'] = 0
+        cherrypy.response.cookie['flash']['path'] = '/'
+        flash = cherrypy.request.cookie.get('flash')
+
         return renderer.render(
             renderer.load_template("layout"),
             content = content,
             logged_in = users.get_current_user() is not None,
             login_url = users.create_login_url(cherrypy.url()),
-            logout_url = users.create_logout_url(cherrypy.url()))
+            logout_url = users.create_logout_url(cherrypy.url()),
+            message = flash and flash.value)
+
+    def flash(self, message):
+        cherrypy.response.cookie['flash'] = message
+        cherrypy.response.cookie['flash']['path'] = '/'
--- handlers/packages.py
+++ handlers/packages.py
@@ -1,4 +1,5 @@
 import cherrypy
+from google.appengine.api import users
 
 import handlers
 
@@ -9,4 +10,11 @@ class Packages(handlers.Base):
 
     @cherrypy.expose
     def create(self):
+        if not users.get_current_user():
+            raise cherrypy.HTTPRedirect(
+                users.create_login_url(cherrypy.url()))
+        elif not users.is_current_user_admin():
+            self.flash('Currently only admins may create packages.')
+            raise cherrypy.HTTPRedirect('/packages')
+
         return self.render("packages/create")
