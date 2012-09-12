8c3a0fee53407190cb0c6747700babc7f36c2536
Nathan Weizenbaum <nweiz@google.com>
2012-09-12 16:05:07 -0700

    Fix some flaws in semantic version ordering.

--- test/test_models/test_semantic_version.py
+++ test/test_models/test_semantic_version.py
@@ -2,6 +2,7 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
+import itertools
 from google.appengine.ext import db
 
 from testcase import TestCase
@@ -30,3 +31,24 @@ class SemanticVersionTest(TestCase):
         assert_invalid_semver("1.3-pre")
         assert_invalid_semver("1.3+build")
         assert_invalid_semver("1.3+bu?!3ild")
+
+    def test_ordering(self):
+        def assert_less_than(version1, version2):
+            self.assertTrue(
+                SemanticVersion(version1) < SemanticVersion(version2),
+                "Expected %s < %s" % (version1, version2))
+
+        # These version numbers come from the semantic versioning spec, located
+        # at http://semver.org/.
+        versions = ["1.0.0-alpha", "1.0.0-alpha.1", "1.0.0-beta.2",
+                    "1.0.0-beta.11", "1.0.0-rc.1", "1.0.0-rc.1+build.1",
+                    "1.0.0", "1.0.0+0.3.7", "1.3.7+build",
+                    "1.3.7+build.2.b8f12d7", "1.3.7+build.11.e0f985a"]
+
+        for version1, version2 in _pairs(versions):
+            assert_less_than(version1, version2)
+
+def _pairs(iterable):
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return itertools.izip(a, b)
