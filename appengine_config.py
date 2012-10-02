2bc85ce122824911fb5c83593fae98821b2075a4
Nathan Weizenbaum <nweiz@google.com>
2012-10-02 16:40:19 -0700

    Partition the database based on namespaces.

---
+++ appengine_config.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+import os
+
+from google.appengine.api import namespace_manager
+
+def namespace_manager_default_namespace_for_request():
+    """Choose which namespace to use for a given request.
+
+    The database, task queue, and memcache are all automatically partitioned
+    based on the current namespace. We use two namespaces: the empty string for
+    the production environment, and "staging" for the staging environment.
+    """
+    version = os.environ.get('CURRENT_VERSION_ID')
+    if version == '1' or version == 'preview': return ""
+    return "staging"
